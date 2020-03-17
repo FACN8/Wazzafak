3883e104974205a6d22f3d8a94c8ddea9cfa8581
Rabea Fahoum <Rabea.fahoum@gmail.com>
2020-03-17 09:54:29 +0200

    Intro skeleton rel #3

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default props => {
-return <div className="main">Intro page!</div>
+return <div className="main">Wazzafak Intro page!
+<button className="unit-button" onClick={() => {}}>I already have an acount</button>
+<button className="unit-button" onClick={() => {}}>Login</button>
+<a href="">I am a business</a>
+</div>
 }

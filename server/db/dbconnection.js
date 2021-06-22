114fbf3e58f7c0b3c5d5100df0290e9de5f0e46c
rabeaf <rabea.fahoum@gmail.com>
2021-06-22 09:26:20 +0300

    fix connection string

--- server/db/dbconnection.js
+++ server/db/dbconnection.js
@@ -3,15 +3,12 @@ require('dotenv').config();
 
 if (!process.env.DATABASE_URL) throw new Error('Enviroment variable DATABASE_URL must be set')
 
+var connectionString = process.env.DATABASE_URL;
 const params = new URL(process.env.DATABASE_URL);
 const options = {
-    host: params.hostname,
-    port: params.port,
-    database: params.pathname.split('/')[1],
-    max: process.env.MAX_CONNECTION || 2,
-    user: params.username,
-    password: params.password,
-    ssl: params.hostname !== 'localhost'
+    connectionString,
+    ssl: { rejectUnauthorized: false }
 }
 
+console.log(options)
 module.exports = new Pool(options);
